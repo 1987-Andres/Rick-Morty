b5843f274684ac45a59e95ebe45fb3619eab8729
Mella <melladjs@hotmail.com>
2022-03-02 13:56:48 +0100

    API Rick & Morty

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  personajes: any[];
+  paginaActual: number;
+  numPaginas: number;
+
+  constructor(private apiService: ApiService) {
+    this.paginaActual = 1;
+  }
+
+  ngOnInit() {
+    this.apiService.getPersonajes()
+      .then(response => {
+        this.personajes = response.results;
+        this.numPaginas = response.info.pages;
+      })
+      .catch(error => console.log(error));
+  }
+
+  async onClick(siguiente: boolean) {
+    if (siguiente) {
+      this.paginaActual++;
+    } else {
+      this.paginaActual--;
+    }
+
+    const response = await this.apiService.getPersonajes(this.paginaActual)
+    this.personajes = response.results;
+
+  }
+
+}
